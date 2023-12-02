2fd24688053442585fed1a3c82b8cf71640a6c3a
Montu Ngoding <132375446+Fadlulihsan0901204@users.noreply.github.com>
2023-12-02 21:28:30 +0700

    Update COD.java
    Menambahkan javadoc

--- COD.java
+++ COD.java
@@ -1,11 +1,27 @@
 import java.util.*;
+
+/**
+ * Kelas COD (Cash On Delivery) merepresentasikan metode pembayaran menggunakan pembayaran tunai saat barang diterima.
+ * Kelas ini mengambil turunan dari kelas Pembayaran dan memberikan rincian
+ * spesifik terkait dengan pembayaran secara langsung saat pengiriman barang.
+ */
 class COD extends Pembayaran {
 
+  /**
+   * Mendapatkan metode pembayaran yang digunakan, yaitu "COD" (Cash On Delivery).
+   *
+   * @return Metode pembayaran sebagai string.
+   */
   @Override
   public String getMetode() {
     return "COD";
   }
 
+  /**
+   * Menampilkan instruksi pembayaran untuk metode pembayaran tunai saat barang diterima (COD).
+   * Termasuk informasi bahwa pembayaran akan dilakukan secara tunai dan
+   * nasihat untuk memastikan ketersediaan uang tunai yang cukup.
+   */
   @Override
   public void tampilkanInstruksiPembayaran() {
     System.out.println("Pembayaran akan dilakukan secara tunai saat barang diterima.");
