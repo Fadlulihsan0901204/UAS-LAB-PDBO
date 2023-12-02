0694b6249a3a7390e209852ed25506ae3bb03d0e
Montu Ngoding <132375446+Fadlulihsan0901204@users.noreply.github.com>
2023-12-02 21:30:34 +0700

    Update Pembayaran.java
    Menambah Javadoc

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,8 +1,21 @@
 import java.util.*;
+
+/**
+ * Kelas abstrak Pembayaran menyediakan kerangka dasar untuk metode pembayaran.
+ * Kelas ini berisi metode-metode abstrak yang harus diimplementasikan oleh kelas turunannya.
+ */
 public abstract class Pembayaran {
-  // Metode abstrak untuk mendapatkan jenis metode pembayaran
+
+  /**
+   * Metode abstrak untuk mendapatkan jenis metode pembayaran.
+   *
+   * @return Jenis metode pembayaran sebagai string.
+   */
   public abstract String getMetode();
 
-  // Metode abstrak untuk menampilkan instruksi pembayaran
+  /**
+   * Metode abstrak untuk menampilkan instruksi pembayaran.
+   * Implementasi metode ini akan bervariasi tergantung pada jenis metode pembayaran.
+   */
   public abstract void tampilkanInstruksiPembayaran();
 }
