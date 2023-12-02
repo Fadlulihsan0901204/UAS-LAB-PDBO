7639b19dde7f895fe7e813a930158b1c7731d4cc
Montu Ngoding <132375446+Fadlulihsan0901204@users.noreply.github.com>
2023-12-02 21:33:20 +0700

    Update Qris.java
    menambahkan javadoc

--- Qris.java
+++ Qris.java
@@ -1,18 +1,33 @@
+/**
+ * Kelas Qris merupakan implementasi metode pembayaran menggunakan QRIS (Quick Response Code).
+ * Kelas ini mengambil turunan dari kelas Pembayaran dan menyediakan rincian
+ * terkait dengan pembayaran melalui QR Code.
+ */
 public class Qris extends Pembayaran {
 
-   @Override
+  /**
+   * Mendapatkan jenis metode pembayaran, yaitu "QRIS".
+   *
+   * @return Jenis metode pembayaran sebagai string.
+   */
+  @Override
   public String getMetode() {
     return "QRIS";
   }
+
+  /**
+   * Menampilkan instruksi pembayaran untuk metode pembayaran menggunakan QRIS.
+   * Instruksi mencakup pesan untuk memindai QR Code yang disediakan.
+   */
   @Override
-   public void tampilkanInstruksiPembayaran(){
-      System.out.println("Scan QR Code Berikut :");
-      System.out.println("||||||||||||||||||||||");
-      System.out.println("||||||||||||    |||| |");
-      System.out.println("|||  | ||||| || ||| ||");
-      System.out.println("|  |||| ||||    ||||| ");
-      System.out.println("|  |||| | |||||||| |||");
-      System.out.println("|||    |||| |||| || ||");
-      System.out.println("|||||||||||||||||| |||");
-   }
+  public void tampilkanInstruksiPembayaran() {
+    System.out.println("Scan QR Code Berikut :");
+    System.out.println("||||||||||||||||||||||");
+    System.out.println("||||||||||||    |||| |");
+    System.out.println("|||  | ||||| || ||| ||");
+    System.out.println("|  |||| ||||    ||||| ");
+    System.out.println("|  |||| | |||||||| |||");
+    System.out.println("|||    |||| |||| || ||");
+    System.out.println("|||||||||||||||||| |||");
+  }
 }
