3b08798aeb7711afa1878fca81d519796a1d7b23
Montu Ngoding <132375446+Fadlulihsan0901204@users.noreply.github.com>
2023-12-02 21:29:33 +0700

    Update ListBarang.java
    Menambah Javadoc

--- ListBarang.java
+++ ListBarang.java
@@ -1,11 +1,22 @@
 import java.util.*;
+
+/**
+ * Kelas ListBarang menyimpan daftar barang dalam bentuk ArrayList.
+ */
 public class ListBarang {
 
+  /**
+   * ArrayList yang menyimpan objek-objek Barang.
+   */
   public ArrayList<Barang> barang;
 
+  /**
+   * Konstruktor untuk kelas ListBarang.
+   * Inisialisasi daftar barang dengan beberapa contoh barang.
+   */
   public ListBarang() {
     this.barang = new ArrayList<>();
-     barang.add(new Barang("Laptop", 10000000, 20));
+    barang.add(new Barang("Laptop", 10000000, 20));
     barang.add(new Barang("Smartphone", 5000000, 15));
     barang.add(new Barang("Headphone", 500000, 30));
     barang.add(new Barang("TV", 20000000, 15));
@@ -16,9 +27,13 @@ public class ListBarang {
     barang.add(new Barang("Mouse", 50000, 50));
     barang.add(new Barang("Keyboard", 80000, 30));
     barang.add(new Barang("Monitor", 1500000, 20));
-   
   }
 
+  /**
+   * Metode untuk memperbarui daftar barang dengan daftar yang diberikan.
+   *
+   * @param updatedList Daftar barang yang baru.
+   */
   public void updateListBarang(ArrayList<Barang> updatedList) {
     this.barang = updatedList;
   }
